c2fa52fd243edcb5da833bdcf09114b61b9aca3e
Richard Abishai <rickoshade1891@gmail.com>
2025-11-27 19:32:09 +0530

    Update Next.js configuration to enable experimental caching features and adjust TypeScript settings for JSX and library paths.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    useCache: true,
+    ppr: true,
+    cacheComponents: true,
+  },
 };
 
 export default nextConfig;
